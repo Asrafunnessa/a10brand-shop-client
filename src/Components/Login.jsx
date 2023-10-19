725f545d70c4af37acb93b7ccea57dc636f4fa07
Asrafunnessa <ayeshapriya0@gmail.com>
2023-10-19 18:06:16 +0600

    set login Page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,49 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
+
     return (
         <div>
+            <div>
+                <h2 className="text-5xl font-bold my-10 text-center">Please Login</h2>
+                <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    <div className="form-control mt-6">
+                <button className="btn btn-outline btn-secondary">
+                    Login With Google
+                </button>
+            </div>
+                </form>
+                <p className="text-center mt-4">Do not have an account Please <Link className="text-blue-600 font-bold" to="/register">Register</Link></p>
+            </div>
+          
             
+
         </div>
     );
 };
